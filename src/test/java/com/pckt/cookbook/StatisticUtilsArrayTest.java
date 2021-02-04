951dc059afdf036978efa6fbf96679b3a6f9bae6
Chrigale <70653899+Chrigale@users.noreply.github.com>
2021-02-04 12:52:16 +0200

    Add files via upload

--- src/test/java/com/pckt/cookbook/StatisticUtilsArrayTest.java
+++ src/test/java/com/pckt/cookbook/StatisticUtilsArrayTest.java
@@ -13,20 +13,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticUtilsArrayTest {
 
+    // Initializing auxiliary variables.
     private StatisticUtilsArray calculator;
 
+    /**
+     * This method is setting up the test environment by creating a calculator object using the void constructor.
+     */
     @BeforeEach
     public void setUpBeforeClass(){
         this.calculator = new StatisticUtilsArray();
     }
 
+    /**
+     * This method is used for testing the getMax() method of the StatisticUtilsArray class.
+     **/
     @ParameterizedTest
     @MethodSource
-    public void testMax(double[] input, double expected) {
-        assertEquals(expected, calculator.getMax(input));
-    }
+    public void testMax(double[] input, double expected) { assertEquals(expected, calculator.getMax(input)); }
 
     static Stream<Arguments> testMax() {
+
         return Stream.of(
                 Arguments.of(new double[] {1.38, 2.56, 4.3}, 4.3),
                 Arguments.of(new double[] {1.38, 2.56, 4.0}, 4.0),
@@ -34,69 +40,89 @@ public class StatisticUtilsArrayTest {
         );
     }
 
+    /**
+     * This method is used for testing the getMin() method of the StatisticUtilsArray class.
+     **/
     @ParameterizedTest
     @MethodSource
-    public void testMin(double[] input, double expected) {
-        assertEquals(expected, calculator.getMin(input));
-    }
+    public void testMin(double[] input, double expected) { assertEquals(expected, calculator.getMin(input)); }
 
     static Stream<Arguments> testMin() {
+
         return Stream.of(
                 Arguments.of(new double[]  {1.38, 2.56, 4.3}, 1.38),
                 Arguments.of(new double[]  {5.0, 8.0, -5.0}, -5.0)
         );
     }
 
+    /**
+     * This method is used for testing the getMean() method of the StatisticUtilsArray class.
+     **/
     @ParameterizedTest
     @MethodSource
-    public void testMean(double[] input, double expected) {
-        assertEquals(expected, calculator.getMean(input));
-    }
+    public void testMean(double[] input, double expected) { assertEquals(expected, calculator.getMean(input)); }
 
     static Stream<Arguments> testMean() {
+
         return Stream.of(
                 Arguments.of(new double[] {9.0, 10.0, 12.0, 13.0, 13.0}, 11.4),
                 Arguments.of(new double[] {0.0, 0.0, 0.0}, 0.0)
         );
     }
 
+    /**
+     * This method is used for testing the getMedian() method of the StatisticUtilsArray class.
+     **/
     @ParameterizedTest
     @MethodSource
-    public void testMedian(double[] input, double expected) {
-        assertEquals(expected, calculator.getMedian(input));
-    }
+    public void testMedian(double[] input, double expected) { assertEquals(expected, calculator.getMedian(input)); }
 
     static Stream<Arguments> testMedian() {
+
         return Stream.of(
                 Arguments.of(new double[] {9.0, 10.0, 12.0, 13.0, 13.0}, 12.0),
                 Arguments.of(new double[] {0.0, 0.0, 0.0}, 0.0)
         );
     }
 
+    /**
+     * This method is used for testing the getStandardDeviation() method of the StatisticUtilsArray class.
+     **/
     @ParameterizedTest
     @MethodSource
-    public void testStandardDeviation(double[] input, double expected) {
-        assertEquals(expected, calculator.getStandardDeviation(input));
-    }
+    public void testStandardDeviation(double[] input, double expected) { assertEquals(expected, calculator.getStandardDeviation(input)); }
 
     static Stream<Arguments> testStandardDeviation() {
+
         return Stream.of(
                 Arguments.of(new double[] {10.0, 12.0, 23.0, 23.0}, 6.97614984548545)
         );
     }
 
+    /**
+     * This method is used for testing the getMax() , getMin() , getMean() , getMedian() , getStandardDeviation() methods of StatisticUtilsArray class
+     * for null and empty inputs.
+     */
     @ParameterizedTest
     @NullAndEmptySource
     void testMaxForNullAndEmptyArrays(double[] input) {
         assertAll(
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getMax(input),
                         "didn't throw an exception in getMax method in StatisticUtilsArray class!"),
+
+
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getMin(input),
                         "didn't throw an exception in getMin method in StatisticUtilsArray class!"),
+
+
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getMean(input),
                         "didn't throw an exception in getMean method in StatisticUtilsArray class!"),
+
+
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getMedian(input),
                         "didn't throw an exception in getMedian method in StatisticUtilsArray class!"),
+
+
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getStandardDeviation(input),
                         "didn't throw an exception in getStandardDeviation method in StatisticUtilsArray class!")
         );
@@ -104,12 +130,18 @@ public class StatisticUtilsArrayTest {
     }
 
 
+    /**
+     * This method is used for testing the getMean() and getStandardDeviation() methods of StatisticUtilsArray class
+     * for inputs that contains infinity as a value.
+     */
     @ParameterizedTest
     @MethodSource
     public void testInfinityExceptions(double[] input) {
         assertAll(
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getMean(input),
                         "didn't throw an infinity exception in getMean method in StatisticUtilsArray class!"),
+
+
                 () -> assertThrows(IllegalArgumentException.class, () -> calculator.getStandardDeviation(input),
                         "didn't throw an infinity exception in getStandardDeviation method in StatisticUtilsArray class!")
         );
@@ -117,6 +149,7 @@ public class StatisticUtilsArrayTest {
     }
 
     static Stream<Arguments> testInfinityExceptions() {
+
         return Stream.of(
                 Arguments.of(new double[] {1.38,POSITIVE_INFINITY, 4.3}),
                 Arguments.of(new double[] {1.38, NEGATIVE_INFINITY, 4.3})
